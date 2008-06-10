0a3a9b6e21d59068b01e1f58a3ffdda060009f14
David M. Lloyd <david.lloyd@redhat.com>
2008-06-10 00:53:08 +0000

    javadoc fix

--- spi/src/main/java/org/jboss/xnio/spi/OneWayPipe.java
+++ spi/src/main/java/org/jboss/xnio/spi/OneWayPipe.java
@@ -4,10 +4,20 @@ import org.jboss.xnio.channels.StreamSourceChannel;
 import org.jboss.xnio.channels.StreamSinkChannel;
 
 /**
- *
+ * A one-way pipe connection between a source handler and a sink handler.
  */
 public interface OneWayPipe extends ExecutorUser, Lifecycle {
+    /**
+     * Get the source end of the pipe.
+     *
+     * @return the source end
+     */
     PipeEnd<StreamSourceChannel> getSourceEnd();
 
+    /**
+     * Get the sink end of the pipe.
+     *
+     * @return the sink end
+     */
     PipeEnd<StreamSinkChannel> getSinkEnd();
 }
