3028f9dbaa8be41753094ce08c832354572c4c63
David M. Lloyd <david.lloyd@redhat.com>
2008-06-11 08:20:05 +0000

    Next version will be CR1

--- version/src/main/java/org/jboss/xnio/Version.java
+++ version/src/main/java/org/jboss/xnio/Version.java
@@ -9,7 +9,7 @@ public final class Version {
     /**
      * The current XNIO version.
      */
-    public static final String VERSION = "1.0.0.Beta1";
+    public static final String VERSION = "1.0.0.CR1";
 
     /**
      * Print out the current XNIO version on {@code System.out}.
