25c98a23b48ad3d408f5f8fa32a09d45f1c77129
David M. Lloyd <david.lloyd@redhat.com>
2008-06-16 16:57:41 +0000

    Add logging javadoc; make logging part of the API...

--- logging-jdk/src/main/java/org/jboss/xnio/log/Logger.java
+++ logging-jdk/src/main/java/org/jboss/xnio/log/Logger.java
@@ -6,7 +6,7 @@ import java.util.logging.LogRecord;
 
 
 /**
- *
+ * A logger that may be used by XNIO applications.
  */
 public final class Logger {
     private static final class Init {
@@ -15,6 +15,9 @@ public final class Logger {
         }
     }
 
+    /**
+     * A base class for the XNIO custom log levels for JDK logging.
+     */
     public static final class Level extends java.util.logging.Level {
         private static final long serialVersionUID = 2595049467798273809L;
 
@@ -23,10 +26,15 @@ public final class Logger {
         }
     }
 
+    /** The TRACE log level. */
     public static final Level TRACE = new Level("TRACE", 400);
+    /** The DEBUG log level. */
     public static final Level DEBUG = new Level("DEBUG", 500);
+    /** The INFO log level. */
     public static final Level INFO = new Level("INFO", 800);
+    /** The WARN log level. */
     public static final Level WARN = new Level("WARN", 900);
+    /** The ERROR log level. */
     public static final Level ERROR = new Level("ERROR", 1000);
 
     @SuppressWarnings ({"NonConstantLogger"})
@@ -40,14 +48,31 @@ public final class Logger {
         logger = java.util.logging.Logger.getLogger(name);
     }
 
+    /**
+     * Get a logger with the given name.
+     *
+     * @param name the logger name
+     * @return the matching logger
+     */
     public static Logger getLogger(final String name) {
         return new Logger(name);
     }
 
+    /**
+     * Get a logger whose name is the same as the fully qualified name of the given class.
+     *
+     * @param claxx the class name
+     * @return the matching logger
+     */
     public static Logger getLogger(final Class claxx) {
         return getLogger(claxx.getName());
     }
 
+    /**
+     * Test to see if the logger would log trace messages.
+     *
+     * @return {@code true} if trace messages may be logged
+     */
     public boolean isTrace() {
         return logger.isLoggable(TRACE);
     }
@@ -69,62 +94,152 @@ public final class Logger {
         }
     }
 
+    /**
+     * Log a message at error level.
+     *
+     * @param msg the message to log
+     */
     public void error(String msg) {
         doLog(ERROR, msg, null, null);
     }
 
+    /**
+     * Log a message at error level with an exception.  The message will be formatted using {@link String#format(String, Object[])}.
+     *
+     * @param ex the exception to log
+     * @param msg the message to log
+     * @param params the message parameters
+     */
     public void error(Throwable ex, String msg, Object... params) {
         doLog(ERROR, msg, ex, params);
     }
 
+    /**
+     * Log a message at error level.  The message will be formatted using {@link String#format(String, Object[])}.
+     *
+     * @param msg the message to log
+     * @param params the message parameters
+     */
     public void error(String msg, Object... params) {
         doLog(ERROR, msg, null, params);
     }
 
+    /**
+     * Log a message at warn level.
+     *
+     * @param msg the message to log
+     */
     public void warn(String msg) {
         doLog(WARN, msg, null, null);
     }
 
+    /**
+     * Log a message at warn level with an exception.  The message will be formatted using {@link String#format(String, Object[])}.
+     *
+     * @param ex the exception to log
+     * @param msg the message to log
+     * @param params the message parameters
+     */
     public void warn(Throwable ex, String msg, Object... params) {
         doLog(WARN, msg, ex, params);
     }
 
+    /**
+     * Log a message at warn level.  The message will be formatted using {@link String#format(String, Object[])}.
+     *
+     * @param msg the message to log
+     * @param params the message parameters
+     */
     public void warn(String msg, Object... params) {
         doLog(WARN, msg, null, params);
     }
 
+    /**
+     * Log a message at info level.
+     *
+     * @param msg the message to log
+     */
     public void info(String msg) {
         doLog(INFO, msg, null, null);
     }
 
+    /**
+     * Log a message at info level with an exception.  The message will be formatted using {@link String#format(String, Object[])}.
+     *
+     * @param ex the exception to log
+     * @param msg the message to log
+     * @param params the message parameters
+     */
     public void info(Throwable ex, String msg, Object... params) {
         doLog(INFO, msg, ex, params);
     }
 
+    /**
+     * Log a message at info level.  The message will be formatted using {@link String#format(String, Object[])}.
+     *
+     * @param msg the message to log
+     * @param params the message parameters
+     */
     public void info(String msg, Object... params) {
         doLog(INFO, msg, null, params);
     }
 
+    /**
+     * Log a message at debug level.
+     *
+     * @param msg the message to log
+     */
     public void debug(String msg) {
         doLog(DEBUG, msg, null, null);
     }
 
+    /**
+     * Log a message at debug level with an exception.  The message will be formatted using {@link String#format(String, Object[])}.
+     *
+     * @param ex the exception to log
+     * @param msg the message to log
+     * @param params the message parameters
+     */
     public void debug(Throwable ex, String msg, Object... params) {
         doLog(DEBUG, msg, ex, params);
     }
 
+    /**
+     * Log a message at debug level.  The message will be formatted using {@link String#format(String, Object[])}.
+     *
+     * @param msg the message to log
+     * @param params the message parameters
+     */
     public void debug(String msg, Object... params) {
         doLog(DEBUG, msg, null, params);
     }
 
+    /**
+     * Log a message at trace level.
+     *
+     * @param msg the message to log
+     */
     public void trace(String msg) {
         doLog(TRACE, msg, null, null);
     }
 
+    /**
+     * Log a message at trace level with an exception.  The message will be formatted using {@link String#format(String, Object[])}.
+     *
+     * @param ex the exception to log
+     * @param msg the message to log
+     * @param params the message parameters
+     */
     public void trace(Throwable ex, String msg, Object... params) {
         doLog(TRACE, msg, ex, params);
     }
 
+    /**
+     * Log a message at trace level.  The message will be formatted using {@link String#format(String, Object[])}.
+     *
+     * @param msg the message to log
+     * @param params the message parameters
+     */
     public void trace(String msg, Object... params) {
         doLog(TRACE, msg, null, params);
     }
