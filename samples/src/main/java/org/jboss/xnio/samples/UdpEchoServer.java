479c5b179d4c05fa1fe4b37a93018b509ffcb2a3
David M. Lloyd <david.lloyd@redhat.com>
2008-06-20 03:34:43 +0000

    UDP echo server

---
+++ samples/src/main/java/org/jboss/xnio/samples/UdpEchoServer.java
@@ -0,0 +1,51 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2008, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.xnio.samples;
+
+import org.jboss.xnio.Xnio;
+import org.jboss.xnio.ConfigurableFactory;
+import org.jboss.xnio.IoUtils;
+import java.io.IOException;
+import java.io.Closeable;
+import java.net.InetSocketAddress;
+
+/**
+ *
+ */
+public final class UdpEchoServer {
+    private UdpEchoServer() {
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final Xnio xnio = Xnio.createNio();
+        final ConfigurableFactory<Closeable> factory = xnio.createUdpServer(false, new UdpEchoServerHandlerFactory(), new InetSocketAddress(10007));
+        final Closeable server = factory.create();
+        try {
+            while (true) {
+                Thread.sleep(1000L);
+            }
+        } finally {
+            IoUtils.safeClose(server);
+        }
+    }
+}
---
+++ samples/src/main/java/org/jboss/xnio/samples/UdpEchoServerHandler.java
@@ -0,0 +1,66 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2008, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.xnio.samples;
+
+import org.jboss.xnio.IoHandler;
+import static org.jboss.xnio.Buffers.flip;
+import org.jboss.xnio.log.Logger;
+import org.jboss.xnio.channels.MulticastDatagramChannel;
+import org.jboss.xnio.channels.MultipointReadResult;
+import java.nio.ByteBuffer;
+import java.io.IOException;
+import java.net.SocketAddress;
+
+
+/**
+ *
+ */
+public final class UdpEchoServerHandler implements IoHandler<MulticastDatagramChannel> {
+    private static final Logger log = Logger.getLogger(UdpEchoServerHandler.class);
+
+    public void handleOpened(final MulticastDatagramChannel channel) {
+        log.info("UDP echo channel opened!");
+        channel.resumeReads();
+    }
+
+    public void handleReadable(final MulticastDatagramChannel channel) {
+        final ByteBuffer buffer = ByteBuffer.allocate(65536);
+        try {
+            final MultipointReadResult<SocketAddress> result = channel.receive(buffer);
+            channel.resumeReads();
+            if (result != null) {
+                flip(buffer);
+                channel.send(result.getSourceAddress(), buffer);
+            }
+        } catch (IOException e) {
+            log.error("Error echoing datagram: %s", e);
+        }
+    }
+
+    public void handleWritable(final MulticastDatagramChannel channel) {
+    }
+
+    public void handleClosed(final MulticastDatagramChannel channel) {
+        log.info("UDP echo channel closed!");
+    }
+}
