38a1a32fcb74344e1984130ca435af57dc1113a9
David M. Lloyd <david.lloyd@redhat.com>
2008-10-13 17:54:54 +0000

    1.1.0.GA, yay

--- version/src/main/java/org/jboss/xnio/Version.java
+++ version/src/main/java/org/jboss/xnio/Version.java
@@ -31,7 +31,7 @@ public final class Version {
     /**
      * The current XNIO version.
      */
-    public static final String VERSION = "1.1.0.CR1";
+    public static final String VERSION = "1.1.0.GA";
 
     /**
      * Print out the current XNIO version on {@code System.out}.
