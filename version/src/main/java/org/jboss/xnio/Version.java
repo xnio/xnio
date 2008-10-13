8f942e0946b10eb79bf31a49004bf237700dced3
David M. Lloyd <david.lloyd@redhat.com>
2008-10-13 19:03:46 +0000

    Next is 1.2.0...

--- version/src/main/java/org/jboss/xnio/Version.java
+++ version/src/main/java/org/jboss/xnio/Version.java
@@ -31,7 +31,7 @@ public final class Version {
     /**
      * The current XNIO version.
      */
-    public static final String VERSION = "1.1.0.GA";
+    public static final String VERSION = "1.2.0.Alpha";
 
     /**
      * Print out the current XNIO version on {@code System.out}.
