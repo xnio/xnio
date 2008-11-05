f3f11919b09fafa1d3ca62371f1db34ec1f2502d
David M. Lloyd <david.lloyd@redhat.com>
2008-11-05 07:54:09 +0000

    More reliable fix

--- nio-impl/src/test/java/org/jboss/xnio/nio/test/NioTcpTestCase.java
+++ nio-impl/src/test/java/org/jboss/xnio/nio/test/NioTcpTestCase.java
@@ -47,6 +47,8 @@ import java.nio.ByteBuffer;
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -65,7 +67,7 @@ public final class NioTcpTestCase extends TestCase {
     private void doConnectionTest(final Runnable body, final IoHandler<? super TcpChannel> clientHandler, final IoHandler<? super TcpChannel> serverHandler) throws Exception {
         synchronized (this) {
             final NioProvider provider = new NioProvider();
-            provider.setConnectionSelectorThreads(2);
+            provider.setExecutor(new ThreadPoolExecutor(4, 10, 1000L, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(50)));
             provider.start();
             try {
                 final NioTcpServer nioTcpServer = new NioTcpServer();
