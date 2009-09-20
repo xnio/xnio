e77a28de7abe8f148196c8f316861d44433cd080
David M. Lloyd <david.lloyd@redhat.com>
2009-09-20 17:47:20 +0000

    Next -> 2.0.0.CR3

--- api/src/main/java/org/jboss/xnio/Version.java
+++ api/src/main/java/org/jboss/xnio/Version.java
@@ -33,7 +33,7 @@ public final class Version {
     /**
      * The current XNIO version.
      */
-    public static final String VERSION = "2.0.0.CR2";
+    public static final String VERSION = "2.0.0.CR3-SNAPSHOT";
 
     /**
      * Print out the current XNIO version on {@code System.out}.
