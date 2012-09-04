9f4b4abd205d867421efa37f8a0dd52a3021007c
David M. Lloyd <david.lloyd@redhat.com>
2012-09-04 17:23:36 -0500

    Half-duplex wrappers for full-duplex channels; use to suppress configuration and to prevent close() from closing both sides

---
+++ api/src/main/java/org/xnio/channels/SpiltStreamSourceChannel.java
@@ -0,0 +1,195 @@
+/*
+ * JBoss, Home of Professional Open Source
+ *
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.xnio.channels;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+import org.xnio.ChannelListener;
+import org.xnio.Option;
+import org.xnio.XnioExecutor;
+import org.xnio.XnioWorker;
+
+import static org.xnio.Bits.*;
+import static org.xnio.ChannelListeners.invokeChannelListener;
+
+/**
+ * A half-duplex (read side) wrapper for a full-duplex channel.
+ *
+ * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
+ */
+public final class SpiltStreamSourceChannel implements StreamSourceChannel {
+    private final StreamSourceChannel delegate;
+
+    private volatile int state;
+
+    private volatile ChannelListener<? super SpiltStreamSourceChannel> readListener;
+    private volatile ChannelListener<? super SpiltStreamSourceChannel> closeListener;
+
+    private static final int FLAG_DELEGATE_CONFIG = 1 << 0;
+    private static final int FLAG_CLOSED = 1 << 1;
+
+    private static final AtomicIntegerFieldUpdater<SpiltStreamSourceChannel> stateUpdater = AtomicIntegerFieldUpdater.newUpdater(SpiltStreamSourceChannel.class, "state");
+
+    /**
+     * Construct a new instance.
+     *
+     * @param delegate the delegate channel
+     * @param delegateConfig {@code true} to delegate configuration, {@code false} otherwise
+     */
+    public SpiltStreamSourceChannel(final StreamSourceChannel delegate, boolean delegateConfig) {
+        this.delegate = delegate;
+        delegate.getReadSetter().set(new ChannelListener<StreamSourceChannel>() {
+            public void handleEvent(final StreamSourceChannel channel) {
+                invokeChannelListener(SpiltStreamSourceChannel.this, readListener);
+            }
+        });
+        state = delegateConfig ? FLAG_DELEGATE_CONFIG : 0;
+    }
+
+    /**
+     * Construct a new instance which doesn't delegate configuration.
+     *
+     * @param delegate the delegate channel
+     */
+    public SpiltStreamSourceChannel(final StreamSourceChannel delegate) {
+        this(delegate, false);
+    }
+
+    /**
+     * Set the read listener.
+     *
+     * @param readListener the read listener
+     */
+    public void setReadListener(final ChannelListener<? super SpiltStreamSourceChannel> readListener) {
+        this.readListener = readListener;
+    }
+
+    /**
+     * Set the close listener.
+     *
+     * @param closeListener the close listener
+     */
+    public void setCloseListener(final ChannelListener<? super SpiltStreamSourceChannel> closeListener) {
+        this.closeListener = closeListener;
+    }
+
+    public ChannelListener.Setter<? extends SpiltStreamSourceChannel> getReadSetter() {
+        return new ChannelListener.Setter<SpiltStreamSourceChannel>() {
+            public void set(final ChannelListener<? super SpiltStreamSourceChannel> listener) {
+                setReadListener(listener);
+            }
+        };
+    }
+
+    public ChannelListener.Setter<? extends SpiltStreamSourceChannel> getCloseSetter() {
+        return new ChannelListener.Setter<SpiltStreamSourceChannel>() {
+            public void set(final ChannelListener<? super SpiltStreamSourceChannel> listener) {
+                setCloseListener(listener);
+            }
+        };
+    }
+
+    public long transferTo(final long position, final long count, final FileChannel target) throws IOException {
+        return delegate.transferTo(position, count, target);
+    }
+
+    public long transferTo(final long count, final ByteBuffer throughBuffer, final StreamSinkChannel target) throws IOException {
+        return delegate.transferTo(count, throughBuffer, target);
+    }
+
+    public int read(final ByteBuffer dst) throws IOException {
+        return delegate.read(dst);
+    }
+
+    public long read(final ByteBuffer[] dsts, final int offset, final int length) throws IOException {
+        return delegate.read(dsts, offset, length);
+    }
+
+    public long read(final ByteBuffer[] dsts) throws IOException {
+        return delegate.read(dsts);
+    }
+
+    public void suspendReads() {
+        delegate.suspendReads();
+    }
+
+    public void resumeReads() {
+        delegate.resumeReads();
+    }
+
+    public void wakeupReads() {
+        delegate.wakeupReads();
+    }
+
+    public boolean isReadResumed() {
+        return delegate.isReadResumed();
+    }
+
+    public void awaitReadable() throws IOException {
+        delegate.awaitReadable();
+    }
+
+    public void awaitReadable(final long time, final TimeUnit timeUnit) throws IOException {
+        delegate.awaitReadable(time, timeUnit);
+    }
+
+    public XnioWorker getWorker() {
+        return delegate.getWorker();
+    }
+
+    public boolean supportsOption(final Option<?> option) {
+        return allAreSet(state, FLAG_DELEGATE_CONFIG) ? delegate.supportsOption(option) : false;
+    }
+
+    public <T> T getOption(final Option<T> option) throws IOException {
+        return allAreSet(state, FLAG_DELEGATE_CONFIG) ? delegate.getOption(option) : null;
+    }
+
+    public <T> T setOption(final Option<T> option, final T value) throws IllegalArgumentException, IOException {
+        return allAreSet(state, FLAG_DELEGATE_CONFIG) ? delegate.setOption(option, value) : null;
+    }
+
+    public void shutdownReads() throws IOException {
+        int oldVal, newVal;
+        do {
+            oldVal = state;
+            if (allAreSet(oldVal, FLAG_CLOSED)) {
+                return;
+            }
+            newVal = oldVal | FLAG_CLOSED;
+        } while (! stateUpdater.compareAndSet(this, oldVal, newVal));
+        delegate.shutdownReads();
+        invokeChannelListener(this, closeListener);
+    }
+
+    public XnioExecutor getReadThread() {
+        return delegate.getReadThread();
+    }
+
+    public boolean isOpen() {
+        return allAreClear(state, FLAG_CLOSED);
+    }
+
+    public void close() throws IOException {
+        shutdownReads();
+    }
+}
---
+++ api/src/main/java/org/xnio/channels/SplitStreamSinkChannel.java
@@ -0,0 +1,217 @@
+/*
+ * JBoss, Home of Professional Open Source
+ *
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.xnio.channels;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+import org.xnio.ChannelListener;
+import org.xnio.Option;
+import org.xnio.XnioExecutor;
+import org.xnio.XnioWorker;
+
+import static org.xnio.Bits.*;
+import static org.xnio.ChannelListeners.invokeChannelListener;
+
+/**
+ * A half-duplex (write side) wrapper for a full-duplex channel.
+ *
+ * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
+ */
+public final class SplitStreamSinkChannel implements StreamSinkChannel {
+    private final StreamSinkChannel delegate;
+
+    private volatile int state;
+
+    private volatile ChannelListener<? super SplitStreamSinkChannel> writeListener;
+    private volatile ChannelListener<? super SplitStreamSinkChannel> closeListener;
+
+    private static final int FLAG_DELEGATE_CONFIG = 1 << 0;
+    private static final int FLAG_CLOSE_REQ = 1 << 1;
+    private static final int FLAG_CLOSE_COMP = 1 << 2;
+
+    private static final AtomicIntegerFieldUpdater<SplitStreamSinkChannel> stateUpdater = AtomicIntegerFieldUpdater.newUpdater(SplitStreamSinkChannel.class, "state");
+
+    /**
+     * Construct a new instance.
+     *
+     * @param delegate the delegate channel
+     * @param delegateConfig {@code true} to delegate configuration, {@code false} otherwise
+     */
+    public SplitStreamSinkChannel(final StreamSinkChannel delegate, boolean delegateConfig) {
+        this.delegate = delegate;
+        state = delegateConfig ? FLAG_DELEGATE_CONFIG : 0;
+        delegate.getWriteSetter().set(new ChannelListener<StreamSinkChannel>() {
+            public void handleEvent(final StreamSinkChannel channel) {
+                invokeChannelListener(SplitStreamSinkChannel.this, writeListener);
+            }
+        });
+    }
+
+    /**
+     * Construct a new instance which does not delegate configuration.
+     *
+     * @param delegate the delegate channel
+     */
+    public SplitStreamSinkChannel(final StreamSinkChannel delegate) {
+        this(delegate, false);
+    }
+
+    /**
+     * Set the write listener.
+     *
+     * @param writeListener the write listener
+     */
+    public void setWriteListener(final ChannelListener<? super SplitStreamSinkChannel> writeListener) {
+        this.writeListener = writeListener;
+    }
+
+    /**
+     * Set the close listener.
+     *
+     * @param closeListener the close listener
+     */
+    public void setCloseListener(final ChannelListener<? super SplitStreamSinkChannel> closeListener) {
+        this.closeListener = closeListener;
+    }
+
+    public ChannelListener.Setter<? extends SplitStreamSinkChannel> getCloseSetter() {
+        return new ChannelListener.Setter<SplitStreamSinkChannel>() {
+            public void set(final ChannelListener<? super SplitStreamSinkChannel> listener) {
+                setCloseListener(listener);
+            }
+        };
+    }
+
+    public ChannelListener.Setter<? extends SplitStreamSinkChannel> getWriteSetter() {
+        return new ChannelListener.Setter<SplitStreamSinkChannel>() {
+            public void set(final ChannelListener<? super SplitStreamSinkChannel> listener) {
+                setWriteListener(listener);
+            }
+        };
+    }
+
+    public void shutdownWrites() throws IOException {
+        int oldVal, newVal;
+        do {
+            oldVal = state;
+            if (allAreSet(oldVal, FLAG_CLOSE_REQ)) {
+                return;
+            }
+            newVal = oldVal | FLAG_CLOSE_REQ;
+        } while (! stateUpdater.compareAndSet(this, oldVal, newVal));
+        delegate.shutdownWrites();
+    }
+
+    public boolean isOpen() {
+        return allAreClear(state, FLAG_CLOSE_REQ);
+    }
+
+    public void close() throws IOException {
+        shutdownWrites();
+        // best lame-duck effort; can't do any more than this really
+        flush();
+    }
+
+    public boolean flush() throws IOException {
+        int oldVal, newVal;
+        oldVal = state;
+        if (allAreSet(oldVal, FLAG_CLOSE_COMP)) {
+            return true;
+        }
+        final boolean flushed = delegate.flush();
+        if (! flushed) {
+            return false;
+        }
+        do {
+            if (allAreClear(oldVal, FLAG_CLOSE_REQ)) {
+                return true;
+            }
+            newVal = oldVal | FLAG_CLOSE_COMP;
+        } while (! stateUpdater.compareAndSet(this, oldVal, newVal));
+        invokeChannelListener(this, closeListener);
+        return true;
+    }
+
+    public long transferFrom(final FileChannel src, final long position, final long count) throws IOException {
+        return delegate.transferFrom(src, position, count);
+    }
+
+    public long transferFrom(final StreamSourceChannel source, final long count, final ByteBuffer throughBuffer) throws IOException {
+        return delegate.transferFrom(source, count, throughBuffer);
+    }
+
+    public int write(final ByteBuffer src) throws IOException {
+        return delegate.write(src);
+    }
+
+    public long write(final ByteBuffer[] srcs, final int offset, final int length) throws IOException {
+        return delegate.write(srcs, offset, length);
+    }
+
+    public long write(final ByteBuffer[] srcs) throws IOException {
+        return delegate.write(srcs);
+    }
+
+    public void suspendWrites() {
+        delegate.suspendWrites();
+    }
+
+    public void resumeWrites() {
+        delegate.resumeWrites();
+    }
+
+    public boolean isWriteResumed() {
+        return delegate.isWriteResumed();
+    }
+
+    public void wakeupWrites() {
+        delegate.wakeupWrites();
+    }
+
+    public void awaitWritable() throws IOException {
+        delegate.awaitWritable();
+    }
+
+    public void awaitWritable(final long time, final TimeUnit timeUnit) throws IOException {
+        delegate.awaitWritable(time, timeUnit);
+    }
+
+    public XnioExecutor getWriteThread() {
+        return delegate.getWriteThread();
+    }
+
+    public XnioWorker getWorker() {
+        return delegate.getWorker();
+    }
+
+    public <T> T setOption(final Option<T> option, final T value) throws IllegalArgumentException, IOException {
+        return allAreSet(state, FLAG_DELEGATE_CONFIG) ? delegate.setOption(option, value) : null;
+    }
+
+    public <T> T getOption(final Option<T> option) throws IOException {
+        return allAreSet(state, FLAG_DELEGATE_CONFIG) ? delegate.getOption(option) : null;
+    }
+
+    public boolean supportsOption(final Option<?> option) {
+        return allAreSet(state, FLAG_DELEGATE_CONFIG) ? delegate.supportsOption(option) : false;
+    }
+}
