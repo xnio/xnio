939df5132e8a5fdf4d8e4aa053aead0757154668
Stuart Douglas <stuart.w.douglas@gmail.com>
2013-02-19 14:50:11 +1100

    A more efficient implementation of FinishedIoFuture

--- api/src/main/java/org/xnio/FinishedIoFuture.java
+++ api/src/main/java/org/xnio/FinishedIoFuture.java
@@ -1,19 +1,28 @@
 
 package org.xnio;
 
+import java.io.IOException;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.TimeUnit;
+
+import static org.xnio.IoFuture.Status.DONE;
+
 /**
  * An implementation of {@link IoFuture} that represents an immediately-successful operation.
  *
  * @param <T> the type of result that this operation produces
  */
-public class FinishedIoFuture<T> extends AbstractIoFuture<T> {
+public class FinishedIoFuture<T> implements IoFuture<T> {
+
+    private final T result;
+
     /**
      * Create an instance.
      *
      * @param result the operation result
      */
     public FinishedIoFuture(T result) {
-        setResult(result);
+        this.result = result;
     }
 
     /**
@@ -22,6 +31,52 @@ public class FinishedIoFuture<T> extends AbstractIoFuture<T> {
      * @return this instance
      */
     public IoFuture<T> cancel() {
+        return this;
+    }
+
+    @Override
+    public Status getStatus() {
+        return DONE;
+    }
+
+    @Override
+    public Status await() {
+        return DONE;
+    }
+
+    @Override
+    public Status await(final long time, final TimeUnit timeUnit) {
+        return DONE;
+    }
+
+    @Override
+    public Status awaitInterruptibly() throws InterruptedException {
+        return DONE;
+    }
+
+    @Override
+    public Status awaitInterruptibly(final long time, final TimeUnit timeUnit) throws InterruptedException {
+        return DONE;
+    }
+
+    @Override
+    public T get() throws IOException, CancellationException {
+        return result;
+    }
+
+    @Override
+    public T getInterruptibly() throws IOException, InterruptedException, CancellationException {
+        return result;
+    }
+
+    @Override
+    public IOException getException() throws IllegalStateException {
+        return null;
+    }
+
+    @Override
+    public <A> IoFuture<T> addNotifier(final Notifier<? super T, A> notifier, final A attachment) {
+        notifier.notify(this, attachment);
         return this;
     }
 }
