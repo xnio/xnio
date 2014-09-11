5694034bf82f932800756885c597284046264be6
David M. Lloyd <david.lloyd@redhat.com>
2014-09-11 09:21:13 -0500

    Do not reuse buffers as locks

--- api/src/main/java/org/xnio/channels/FramedMessageChannel.java
+++ api/src/main/java/org/xnio/channels/FramedMessageChannel.java
@@ -45,6 +45,9 @@ public class FramedMessageChannel extends TranslatingSuspendableChannel<Connecte
     private final Pooled<ByteBuffer> receiveBuffer;
     private final Pooled<ByteBuffer> transmitBuffer;
 
+    private final Object readLock = new Object();
+    private final Object writeLock = new Object();
+
     /**
      * Construct a new instance.
      *
@@ -75,7 +78,7 @@ public class FramedMessageChannel extends TranslatingSuspendableChannel<Connecte
 
     /** {@inheritDoc} */
     public int receive(final ByteBuffer buffer) throws IOException {
-        synchronized (receiveBuffer) {
+        synchronized (readLock) {
             if (isReadShutDown()) {
                 return -1;
             }
@@ -141,7 +144,7 @@ public class FramedMessageChannel extends TranslatingSuspendableChannel<Connecte
 
     /** {@inheritDoc} */
     public long receive(final ByteBuffer[] buffers, final int offs, final int len) throws IOException {
-        synchronized (receiveBuffer) {
+        synchronized (readLock) {
             if (isReadShutDown()) {
                 return -1;
             }
@@ -200,7 +203,7 @@ public class FramedMessageChannel extends TranslatingSuspendableChannel<Connecte
     }
 
     protected void shutdownReadsAction(final boolean writeComplete) throws IOException {
-        synchronized (receiveBuffer) {
+        synchronized (readLock) {
             log.tracef("Shutting down reads on %s", this);
             try {
                 receiveBuffer.getResource().clear();
@@ -216,7 +219,7 @@ public class FramedMessageChannel extends TranslatingSuspendableChannel<Connecte
 
     /** {@inheritDoc} */
     public boolean send(final ByteBuffer buffer) throws IOException {
-        synchronized (transmitBuffer) {
+        synchronized (writeLock) {
             if (isWriteShutDown()) {
                 throw msg.writeShutDown();
             }
@@ -248,7 +251,7 @@ public class FramedMessageChannel extends TranslatingSuspendableChannel<Connecte
 
     /** {@inheritDoc} */
     public boolean send(final ByteBuffer[] buffers, final int offs, final int len) throws IOException {
-        synchronized (transmitBuffer) {
+        synchronized (writeLock) {
             if (isWriteShutDown()) {
                 throw msg.writeShutDown();
             }
@@ -301,13 +304,13 @@ public class FramedMessageChannel extends TranslatingSuspendableChannel<Connecte
     }
 
     protected boolean flushAction(final boolean shutDown) throws IOException {
-        synchronized (transmitBuffer) {
+        synchronized (writeLock) {
             return (doFlushBuffer()) && channel.flush();
         }
     }
 
     protected void shutdownWritesComplete(final boolean readShutDown) throws IOException {
-        synchronized (transmitBuffer) {
+        synchronized (writeLock) {
             log.tracef("Finished shutting down writes on %s", this);
             try {
                 transmitBuffer.free();
@@ -317,7 +320,7 @@ public class FramedMessageChannel extends TranslatingSuspendableChannel<Connecte
     }
 
     private boolean doFlushBuffer() throws IOException {
-        assert Thread.holdsLock(transmitBuffer);
+        assert Thread.holdsLock(writeLock);
         final ByteBuffer buffer = transmitBuffer.getResource();
         buffer.flip();
         try {
@@ -342,7 +345,7 @@ public class FramedMessageChannel extends TranslatingSuspendableChannel<Connecte
     protected void closeAction(final boolean readShutDown, final boolean writeShutDown) throws IOException {
         boolean error = false;
         if (! writeShutDown) {
-            synchronized (transmitBuffer) {
+            synchronized (writeLock) {
                 try {
                     if (! doFlush()) error = true;
                 } catch (Throwable t) {
@@ -355,7 +358,7 @@ public class FramedMessageChannel extends TranslatingSuspendableChannel<Connecte
             }
         }
         if (! readShutDown) {
-            synchronized (receiveBuffer) {
+            synchronized (readLock) {
                 try {
                     receiveBuffer.free();
                 } catch (Throwable t) {
