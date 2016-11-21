3236d9ec642a566405c7be9e835de55db170a987
Tomaz Cerar <tomaz.cerar@gmail.com>
2016-11-21 17:21:42 +0100

    XNIO-268 QueuedNioTcpServer always returns "-1" for "ConnectionCount" attribute in MBean

--- nio-impl/src/main/java/org/xnio/nio/QueuedNioTcpServer.java
+++ nio-impl/src/main/java/org/xnio/nio/QueuedNioTcpServer.java
@@ -35,6 +35,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
@@ -213,7 +214,10 @@ final class QueuedNioTcpServer extends AbstractNioChannel<QueuedNioTcpServer> im
             }
 
             public int getConnectionCount() {
-                return handle.getConnectionCount();
+                CompletableFuture<Integer> future = CompletableFuture.supplyAsync(
+                        () -> openConnections, handle.getWorkerThread()
+                );
+                return future.getNow(-1);
             }
 
             public int getConnectionLimitHighWater() {
