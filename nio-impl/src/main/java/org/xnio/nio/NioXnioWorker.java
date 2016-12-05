29264666a1fdefb7a64bfa0343e808a6e74bb389
Osipov Artemy <osipov.artemy@gmail.com>
2016-12-05 15:52:41 +0300

    XNIO-283 add BusyWorkerThreadCount metric

--- nio-impl/src/main/java/org/xnio/nio/NioXnioWorker.java
+++ nio-impl/src/main/java/org/xnio/nio/NioXnioWorker.java
@@ -406,6 +406,10 @@ final class NioXnioWorker extends XnioWorker {
             return NioXnioWorker.this.getMaxWorkerPoolSize();
         }
 
+        public int getBusyWorkerThreadCount() {
+            return NioXnioWorker.this.getBusyWorkerThreadCount();
+        }
+
         public int getIoThreadCount() {
             return NioXnioWorker.this.getIoThreadCount();
         }
