a2c6b646e2a94e10319d4eb09ccf99cdfac06cdb
Masafumi Miura <mmiura@redhat.com>
2020-08-05 23:03:03 +0900

    [XNIO-376] Improve WorkerThreadFactory to make a created thread execute tasks on thread exits
    - Improve WorkerThreadFactory to make a created thread execute
      handleThreadExit() on the task worker thread exits.
    - Add a new handleThreadExit() API in Xnio. This will be executed on
      thread exits if the thread is created through WorkerThreadFactory.

--- api/src/main/java/org/xnio/XnioWorker.java
+++ api/src/main/java/org/xnio/XnioWorker.java
@@ -1273,7 +1273,16 @@ public abstract class XnioWorker extends AbstractExecutorService implements Conf
         public Thread newThread(final Runnable r) {
             return doPrivileged(new PrivilegedAction<Thread>() {
                 public Thread run() {
-                    final Thread taskThread = new Thread(threadGroup, r, name + " task-" + getNextSeq(), stackSize);
+                    final Thread taskThread = new Thread(threadGroup, new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                r.run();
+                            } finally {
+                                xnio.handleThreadExit();
+                            }
+                        }
+                    }, name + " task-" + getNextSeq(), stackSize);
                     // Mark the thread as daemon if the Options.THREAD_DAEMON has been set
                     if (markThreadAsDaemon) {
                         taskThread.setDaemon(true);
